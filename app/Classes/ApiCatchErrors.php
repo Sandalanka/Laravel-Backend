919d170610f2096708901804dd168db1a355df72
Sandalanka <1415sandalanka@gmail.com>
2024-03-16 09:45:54 +0530

    get by id issues fixing

--- app/Classes/ApiCatchErrors.php
+++ app/Classes/ApiCatchErrors.php
@@ -4,7 +4,6 @@ namespace App\Classes;
 
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\DB;
-
 class ApiCatchErrors{
     
     public static function rollback($e, $message ="Something went wrong! Process not completed"){
@@ -13,7 +12,6 @@ class ApiCatchErrors{
     }
 
     public static function throw($e, $message ="Something went wrong! Process not completed"){
-        
         throw new HttpResponseException(response()->json(["message"=> $message], 500));
     }
 }
